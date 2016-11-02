36f77ee43aa19eba4f32de69deba50dee4dad6b7
TomGoldberg <tomg3210@gmail.com>
2016-11-02 18:58:28 +0200

    font-face new file, replacing new file to free slot

--- Retrieval/test.php
+++ Retrieval/test.php
@@ -1,44 +1,37 @@
 <?php
-
     $filesCounter = 1;
+    $interval = 0;
 
-    // craate sql connection
-    include('/includes/connection.php');
-
-    // make query to sql files table
-    $query = "select * from Files";
-
-    // send query to sql files table
-    $result = mysqli_query($connection , $query);
-    if( !$result ){
-        echo "DB query failed from file_bars.php";
-        die("DB query failed from file_bars.php");
-    }
-
-    // print all sql data rows
-    while( $row = mysqli_fetch_assoc($result) )
-        $filesCounter++;
-
-    // close sql connection
-    mysqli_free_result($result);
-    mysqli_close($connection);
-
+    // full folder address
     $checkTest = getcwd()."/data/";
+    $files1 = scandir($checkTest);
 
+    // display
+    print_r('sizeof($files1) : '.sizeof($files1).'<br>');
+    print_r('$interval : '.$interval.'<br>');
+
+    // search for free slot
+    for( $interval ; $interval < sizeof($files1) ; $interval++){
+        if(!filter_var($files1[$interval], FILTER_SANITIZE_NUMBER_INT)) continue;
+        print_r('$files1['.$interval.'] : '.$files1[$interval].'<br>');
+        if( filter_var($files1[$interval], FILTER_SANITIZE_NUMBER_INT) != $filesCounter){
+            break;
+        }
+        else $filesCounter++;
+    }
+    print_r('available free slot : text'.$filesCounter.'.txt');
+
+    // get the file name to include
     $filename = $_GET['filename'];
 
+    // get file from path
     $path = getcwd()."/source/".$filename."";
+
+    // creating new file
     $newfile = getcwd()."/data/file".$filesCounter.".txt";
 
-    if(!file_exists( $newfile ))
-    {
+    if(!file_exists( $newfile )){
         copy($path, $newfile);
         return;
     }
-    else
-    {
-
-    }
-
-
 ?>
